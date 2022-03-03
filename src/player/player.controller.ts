21b0a1069049fbb6340d96a4979da044d822a286
Kevin <hellokevinvogel@gmail.com>
2022-03-03 21:16:10 +0100

    chore(): add alias

--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -1,4 +1,13 @@
+import { Get } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { wait } from '@/common/helper/utils.helper';
 
 @Controller('player')
-export class PlayerController {}
+export class PlayerController {
+  @Get()
+  public async getPlayer(): Promise<any> {
+    await wait(1000);
+
+    return {};
+  }
+}
