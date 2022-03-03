433714008cb185e1a2f803eb97866a1bdffe1a9f
Kevin <hellokevinvogel@gmail.com>
2022-03-03 22:22:14 +0100

    chore(): add helper

--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -1,13 +1,16 @@
 import { Get } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+// import { randomInt } from '../common/helper/utils.helper'; <- would be the normal way
 import { randomInt } from '@Helper/utils.helper';
-// import { randomInt } from '@/common/helper/utils.helper'; // this works too
-// import { randomInt } from 'src/common/helper/utils.helper'; // original
+// import { randomInt } from '../common/helper/utils.helper'; <- would be the normal way
+import { wait } from '@/common/helper/utils.helper';
 
 @Controller('player')
 export class PlayerController {
   @Get()
   public async getPlayer(): Promise<number> {
+    await wait(1000);
+
     return randomInt(0, 100);
   }
 }
